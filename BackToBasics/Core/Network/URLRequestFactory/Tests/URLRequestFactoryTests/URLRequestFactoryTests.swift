176b367670786b9f3b4311fffb106d25452dbf38
AlexApostol <apostol516@gmail.com>
2024-02-04 19:01:06 +0100

    Add urlRequestFactory

--- BackToBasics/Core/Network/URLRequestFactory/Tests/URLRequestFactoryTests/URLRequestFactoryTests.swift
+++ BackToBasics/Core/Network/URLRequestFactory/Tests/URLRequestFactoryTests/URLRequestFactoryTests.swift
@@ -1,8 +1,23 @@
 import XCTest
 @testable import URLRequestFactory
 
+public final class URLRequestFactory {
+    func makeRequest(with url: URL) -> URLRequest {
+        URLRequest(url: url)
+    }
+}
+
 final class URLRequestFactoryTests: XCTestCase {
     func testExample() throws {
-        XCTFail()
+        //given
+        let sut = makeSUT()
+        let mockURL = URL(string: "https://dummyjson.com")!
+        //when
+        //then
+        XCTAssertEqual(sut.makeRequest(with: mockURL).url?.absoluteURL, mockURL.absoluteURL)
+    }
+    
+    private func makeSUT() -> URLRequestFactory {
+        return URLRequestFactory()
     }
 }
